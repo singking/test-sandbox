737d882f3716dd06871d3a0d79fedb852c3da31d
Tyrone King <kingt@tyrone-mbp.local>
2016-03-18 15:16:59 +0000

    new java 8 features

---
+++ src/lamdas/example2/FunctionalDefaultMethods.java
@@ -0,0 +1,9 @@
+package lamdas.example2;
+
+@FunctionalInterface
+public interface FunctionalDefaultMethods {
+    void method();
+         
+    default void defaultMethod() {           
+    }       
+}
---
+++ src/threading/CyclicBarrierTest.java
@@ -0,0 +1,20 @@
+package threading;
+
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierTest implements Runnable{
+
+	private CyclicBarrier barrier;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+
+		
+	}
+
+}
